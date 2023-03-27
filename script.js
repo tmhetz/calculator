80849b550b4a406220da6c1333e58b2409e401be
Thomas Hetzel <tmhetz@gmail.com>
2023-03-26 22:05:56 -0700

    Create the operate function and related variables

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ function multiply(x, y){
 function divide(x, y){
     return x / y;
 }
+
+let operandOne;
+let operandTwo;
+let operator;
+
+function operate (operandOne, operandTwo, operator){
+    if(operator === "+"){
+        return add(operandOne, operandTwo);
+    } else if(operator === "-"){
+        return subtract(operandOne, operandTwo);
+    } else if(operator === "*"){
+        return multiply(operandOne, operandTwo);
+    } else if(operator === "/"){
+        return divide(operandOne, operandTwo);
+    }
+}
